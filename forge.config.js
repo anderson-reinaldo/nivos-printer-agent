730e3565bfbe423505832f1b7845e917ff776668
REINALDO <andersonnreinaldo@gmail.com>
2025-08-01 16:26:58 -0300

    up forge

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './assets/icons/icon',
+    icon: './assets/icon_no_shape',
     ignore: [
       /^\/tests?/,
       /^\/docs?/,
