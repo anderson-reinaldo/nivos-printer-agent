2d714fc3690d8537c26e95b4f10385c97c843d1a
REINALDO <andersonnreinaldo@gmail.com>
2025-08-01 16:20:27 -0300

    up forge

--- electron/tray.ts
+++ electron/tray.ts
@@ -7,7 +7,7 @@ let tray: Tray | null = null;
 export function createAppTray() {
   if (tray) return tray;
 
-  const iconPath = path.join(__dirname, '../assets/icons/linux/icon.png');
+  const iconPath = path.join(__dirname, '../assets/icon_shape.png');
   let trayIcon: Electron.NativeImage;
 
   try {
